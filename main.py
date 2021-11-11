9a87734c984f026d40acfe6031a283943d880fa1
LH216 <73730357+LH216@users.noreply.github.com>
2021-11-11 18:46:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ def socket_server(a):
     print("等待图片数据")
     conn, addr = socket_client.accept()
     print('-1')
+    b = '%d' %a
+    print(b)
     while True:
         #try:
             # 接收的数据大小,建议比图片本身大,不然无法传输
@@ -58,13 +60,13 @@ def socket_server(a):
                 file_name = str(time_e) + str(zz) + '.jpg'
                 #filename, filesize = struct.unpack(file_name, buf)
                 #fn = file_name.strip('\000')
-                new_filename = os.path.join('./{}'.format(a) + file_name)
+                new_filename = os.path.join('./{}'.format(b) + file_name)
                 print('3')
                 fp = open(new_filename, 'wb')
                 print('start receiving...')
                 fp.write(buf)
                 print('send over')
-                shutil.copyfile(file_name, '{}/{}'.format(a, file_name))
+                shutil.copyfile(file_name, '{}/{}'.format(b, file_name))
                 os.remove('{}'.format(file_name))
                 buf = 0
                 '''
@@ -84,7 +86,7 @@ def socket_server(a):
                 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
                 videoWriter = cv2.VideoWriter('{}.mp4'.format(a), fourcc, fps, (384, 288))
                 # 图片地址
-                imgs = glob.glob('{}/*.jpg'.format(a))
+                imgs = glob.glob('{}/*.jpg'.format(b))
                 for imgname in imgs:
                     frame = cv2.imread(imgname)
                     videoWriter.write(frame)
